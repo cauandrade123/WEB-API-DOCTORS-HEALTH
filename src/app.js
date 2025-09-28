b59edc53346944e7acf3873e93616fd3591b7e43
Cauã Andrade <caua.goldenboyy@gmail.com>
2025-09-27 22:36:53 -0300

    fix: importações equivocadas

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import adicionarRotas from './rotas.js'
+import adicionarRotas from '../src/routes/rotas.js'
 
 const servidor =  express()
 
